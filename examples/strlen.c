7b873956483553efeef6026d27eb83f08be81ed5
LukasPio <lucaspio.galvao@gmail.com>
2026-07-27 23:54:19 -0300

    Add example for strlen function in new strlen.c file

---
+++ examples/strlen.c
@@ -0,0 +1,12 @@
+#include "string.h"
+#include <stdio.h>
+
+int main() {
+
+    char *s = "Testing strlen :)";
+
+    size_t stringLength = strlen(s);
+    printf("Size of received string: %zu\n", stringLength);
+
+    return 0;
+}
