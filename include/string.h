d58c2b2cca01f3edfd7cee0fe346178dec390b29
LukasPio <lucaspio.galvao@gmail.com>
2026-07-28 12:07:30 -0300

    Implement strcmp function in string module

--- include/string.h
+++ include/string.h
@@ -2,3 +2,4 @@
 
 size_t strlen(const char *s);
 size_t strnlen(const char *s, size_t maxlen);
+int strcmp(const char *s1, const char *s2);
--- src/string.c
+++ src/string.c
@@ -23,3 +23,13 @@ size_t strnlen(const char *s, size_t maxlen)
 
     return i;
 }
+
+int strcmp(const char *s1, const char *s2) {
+    
+    while (*s1 == *s2 && *s1 != '\0') {
+        s1++;
+        s2++;
+    }
+
+    return (unsigned char)*s1 - (unsigned char)*s2;
+}
